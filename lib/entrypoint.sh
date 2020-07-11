53a41eb278ea3c3f04657c9aaceaf7d5499b225e
Dark Hole <dark.hole1@yandex.ru>
2020-07-11 15:03:37 +0300

    Fix incorrect variable name

--- lib/entrypoint.sh
+++ lib/entrypoint.sh
@@ -4,4 +4,4 @@ set -e
 
 cd "${2:-.}" || echo "source root not found"
 
-NODE_MODULES="${3:-node_modules}" GITHUB_TOKEN="${GITHUB_TOKEN:-${1:-.}}" SOURCE_ROOT=${2:-.} node /action/lib/run.js
+NODE_PATH="${3:-node_modules}" GITHUB_TOKEN="${GITHUB_TOKEN:-${1:-.}}" SOURCE_ROOT=${2:-.} node /action/lib/run.js
