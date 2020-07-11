ef5936bc084320e87314c78c366406445fc1300d
Dark Hole <dark.hole1@yandex.ru>
2020-07-11 14:43:13 +0300

    Let node find node_modules by himself

--- lib/entrypoint.sh
+++ lib/entrypoint.sh
@@ -4,4 +4,4 @@ set -e
 
 cd "${2:-.}" || echo "source root not found"
 
-NODE_PATH=node_modules GITHUB_TOKEN="${GITHUB_TOKEN:-${1:-.}}" SOURCE_ROOT=${2:-.} node /action/lib/run.js
+GITHUB_TOKEN="${GITHUB_TOKEN:-${1:-.}}" SOURCE_ROOT=${2:-.} node /action/lib/run.js
